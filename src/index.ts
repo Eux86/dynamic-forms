34ff8ea272d78f810d169a5c2409dfdb70c74c59
Eux86 <ditullioeugenio@gmail.com>
2020-02-16 13:10:17 +0000

    fix: exported paths

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export { default as DynamicForm } from './dynamic-form';
+import { DynamicForm } from './dynamic-form';
+
+export default DynamicForm;
 export * from './dynamic-form/models/dynamic-form-schema';
 export * from './dynamic-form/models/event-types';
