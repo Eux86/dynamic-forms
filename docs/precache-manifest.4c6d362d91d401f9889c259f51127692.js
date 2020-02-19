287aa1957b624d80601a1deab21fe2c761a28c17
Eugenio Di Tullio <ditullioeugenio@gmail.com>
2020-02-20 00:34:49 +0100

    chore: updating docs

---
+++ docs/precache-manifest.4c6d362d91d401f9889c259f51127692.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13465fb0a7ad5bef61a48cc4dd69ed5c",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.0c1108c607fa379940bd.bundle.js"
+  },
+  {
+    "url": "runtime~main.0c1108c607fa379940bd.bundle.js"
+  },
+  {
+    "url": "vendors~main.0c1108c607fa379940bd.bundle.js"
+  },
+  {
+    "url": "vendors~main.0c1108c607fa379940bd.bundle.js.LICENSE"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.356001145cfeedc9f5daa16a6b579fc5.js"
+  "precache-manifest.4c6d362d91d401f9889c259f51127692.js"
 );
 
 self.addEventListener('message', (event) => {
